02491595e036ca7b5ed41ffed932f6f88d00604a
rahathosen <rahathosen.me@gmail.com>
2023-10-31 00:00:11 +0600

    revalidate

--- lib/getGQL.ts
+++ lib/getGQL.ts
@@ -1,6 +1,7 @@
 async function fetchGraphQL(query: string): Promise<any> {
   const response = await fetch("https://django-news-server.vercel.app/gql/", {
-    cache: "no-cache",
+    cache: "force-cache",
+    next: { revalidate: 300 },
     method: "POST",
     headers: {
       "Content-Type": "application/json",
