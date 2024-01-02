934345f40555098ce3544ff047565f203b11097e
rahathosen <rahathosen.me@gmail.com>
2024-01-02 18:57:39 +0600

    up

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // Enable React strict mode for improved error handling
-  swcMinify: true,      // Enable SWC minification for improved performance
-  compiler: {
-    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
-  },
   images: {
     remotePatterns: [
       {
@@ -39,12 +34,6 @@ const nextConfig = {
   },
 };
 
-// Configuration object tells the next-pwa plugin 
-const withPWA = require("next-pwa")({
-  dest: "public", // Destination directory for the PWA files
-  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
-  register: true, // Register the PWA service worker
-  skipWaiting: true, // Skip waiting for service worker activation
-});
 
-module.exports = withPWA(nextConfig);
+
+module.exports = nextConfig;
